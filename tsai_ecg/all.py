73c65aea6c761076deeeb2927b31c4302a34f259
williamsdoug <ddwilli@gmail.com>
2021-04-16 11:51:22 -0400

    Complete Config

---
+++ tsai_ecg/all.py
@@ -0,0 +1,10 @@
+import tsai_ecg
+from .imports import *
+# from .utils import *
+# from .data.all import *
+# from .metrics import *
+# from .learner import *
+# from .tslearner import *
+# from .optimizer import *
+# from .models.all import *
+# from .callback.all import *
---
+++ tsai_ecg/imports.py
@@ -0,0 +1,6 @@
+import tsai
+from tsai.all import *
+from tsai.imports import save_nb, last_saved, beep, create_scripts, Timer, PATH, device, cpus, IS_COLAB, timer
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
